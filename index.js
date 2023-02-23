df73f8f22e49d2d92148ba8ab78352285fe2362f
Mika Stenberg <mika.stenberg@laurea.fi>
2023-02-23 10:20:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ http
       response.writeHead(200, { "Content-Type": "text/plain" });
 
       // Lähetetään tekstimuotoinen vastaus selaimelle
-      response.write("Olet saapunut palvelimen juureen.");
+      response.write("Olet saapunut palvelimen juureen - TAAS!");
     } else if (request.url === "/helloworld") {
       // Valitaan Content-type tarjoiltavan sisällön suhteen
       response.writeHead(200, { "Content-Type": "text/html" });
